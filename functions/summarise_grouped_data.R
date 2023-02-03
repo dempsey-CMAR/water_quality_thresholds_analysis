232603f414007f6f489a560acf7aa5052572b0a0
Danielle Dempsey <ddempsey@perennia.ca>
2023-02-03 08:22:14 -0400

    add documentation

--- functions/summarise_grouped_data.R
+++ functions/summarise_grouped_data.R
@@ -1,4 +1,14 @@
-library(dplyr)
+# February 1, 2023
+
+# calculates the mean, standard deviation, and number of rows for
+# each group as specified in ...
+
+# dat: dataframe with at least 1 column, named VALUE. Additional columns can be
+## used for grouping the summary results.
+
+# group_name: character string indicating the name of the group.
+
+# ...: name(s) of grouping column(s)
 
 summarise_grouped_data <- function(dat, group_name, ...) {
 
